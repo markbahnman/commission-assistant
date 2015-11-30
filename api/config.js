13cd259cc15e6f42d354a56b1558ee60380a003b
= <mark.bahnman@gmail.com>
2015-11-30 02:17:17 -0800

    Update config for RDS env variables

--- api/config.js
+++ api/config.js
@@ -1,34 +1,34 @@
 module.exports = {
   'development': {
-    'username': 'markbahnman',
-    'password': null,
-    'database': 'assistant_dev',
+    'username': process.env.RDS_USERNAME || 'markbahnman',
+    'password': process.env.RDS_PASSWORD || null,
+    'database': process.env.RDS_DB_NAME || 'assistant_dev',
     'options': {
       'dialect': 'postgres',
-      'host': 'localhost',
-      'post': 5432,
+      'host': process.env.RDS_HOSTNAME || 'localhost',
+      'port': process.env.RHD_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
       'native': true
     }
   },
   'testing': {
-    'username': 'markbahnman',
+    'username': process.env.RDS_USERNAME || 'markbahnman',
     'password': null,
     'database': 'assistant_test',
     'options': {
       'logging': false,
       'host': 'localhost',
-      'post': 5432,
+      'port': 5432,
       'dialect': 'postgres',
       'native': true
     }
   },
   'production': {
-    'username': 'markbahnman',
-    'password': null,
-    'database': 'assistant_prod',
+    'username': process.env.RDS_USERNAME || 'markbahnman',
+    'password': process.env.RDS_PASSWORD || null,
+    'database': process.env.RDS_DB_NAME || 'assistant_prod',
     'options': {
-      'host': 'localhost',
-      'post': 5432,
+      'host': process.env.RDS_HOSTNAME || 'localhost',
+      'port': process.env.RDS_PORT ? parseInt(process.env.RDS_PORT, 10) : 5432,
       'dialect': 'postgres',
       'native': true
     }
