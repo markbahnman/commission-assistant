e552001428771918fb1154f29e7906ff361ad16d
= <mark.bahnman@gmail.com>
2015-12-01 17:45:15 -0800

    Add ebs script

--- src/config.js
+++ src/config.js
@@ -14,9 +14,9 @@ const environment = {
 
 module.exports = Object.assign({
   host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 8080,
+  port: process.env.PORT,
   apiHost: process.env.APIHOST || 'localhost',
-  apiPort: process.env.APIPORT || process.env.PORT || 8081,
+  apiPort: process.env.APIPORT || process.env.PORT,
   app: {
     title: 'Commission Assistant',
     description: 'Modern Tool for Modern Artists',
