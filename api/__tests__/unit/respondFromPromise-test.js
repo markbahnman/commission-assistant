9e80142aa8a11558666485890edb137f7cf03f15
= <mark.bahnman@gmail.com>
2015-12-02 21:02:08 -0800

    Add respondFromPromise util, tests, etc

---
+++ api/__tests__/unit/respondFromPromise-test.js
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+import respond from '../../utils/respondFromPromise';
+
+describe('respondFromPromise', () => {
+  it('does something', () => {
+    expect(1 == 1);
+  });
+});
---
+++ api/utils/respondFromPromise.js
@@ -0,0 +1,22 @@
+// Resolves promise and returns a status code, redirect location, data payload, and error object
+export default function respond(promise) {
+  return (req, res) => {
+    promise(req, res)
+    .then((result) => {
+      if (result instanceof Function) {
+        result(res);
+      } else {
+        res.status(result.status || 200).json(result);
+      }
+    }, (reason) => {
+      if (reason && reason.redirect) {
+        res.redirect(reason.redirect);
+      } else {
+        if (process.env.NODE_ENV !== 'testing') {
+          console.error('API ERROR:', pretty.render(reason));
+        }
+        res.status(reason.status || 500).json(reason);
+      }
+    });
+  };
+}
